4595eed94cd6c422aab62801482a8b0c6f5671b7
izzatajan <114302064+izzatajan@users.noreply.github.com>
2024-03-16 07:22:03 +0500

    Add files via upload

---
+++ live-search/index.js
@@ -0,0 +1,415 @@
+let quesans = [
+    {
+        question: "2023-yilga yurtimizda qanday nom berildi?",
+        answer: '"Insonga e\'tibor va sifatli ta\'lim" yili'
+      },
+      {
+        question: "2023-yildagi ustuvor yoʻnalishlardan ikkinchi yoʻnalish nomini toping.",
+        answer: 'Ikkinchi yo\'nalish Yangi O\'zbekistonni "ijtimoiy davlat" tamoyili asosida qurishda Konstitutaiyani mustahkamlash.'
+      },
+      {
+        question: "2023-yildagi ustuvor yoʻnalishlardan birinchi yoʻnalishni toping.",
+        answer: 'Birinchi yo\'nalish ixcham va samarali davlat boshqaruv tizimiga o\'tish.'
+      },
+      {
+        question: "Oʻzbekiston Respublikasi Prezidenti Sh.M. Mirziyoyevning Oliy Majlis va Oʻzbekiston xalqiga Murojaatnomasida vazirlik va idoralar soni qanchadan nechtaga kamaytirildi?",
+        answer: '61 tadan 28 tagacha kamaytiriladi, davlat xizmatchilari soni bosqichma-bosqich 25−30 foizga qisqaradi.'
+      },
+      {
+        question: "Soʻnggi yillarda oliygohlarimiz qancha barobarga koʻpayib, nechtaga yetdi, qamrov darajasi necha foizdan necha foizgacha oshdi?",
+        answer: '“Soʻnggi yillarda oliygohlarimiz 2,5 barobar koʻpayib, 198 taga yetdi, qamrov darajasi 9 foizdan 38 foizga oshdi.'
+      },
+      {
+        question: "Dinning ijtimoiy ildizlari?",
+        answer: 'Dinning ijtimoiy - dinni vujudga keltirgan kundalik hayotiy shart-sharoitlar yig\'indisi tushuniladi.'
+      },
+      {
+        question: "Diniy tashkilot nima?",
+        answer: 'Diniy tashkilot - diniy ehtiyojlarni birgalikda qondirish yoki qondirishga koʻmaklashish maqsadida tuziladigan va diniy marosimlarni ado etish asosida ish koʻradigan ixtiyoriy, teng huquqli va oʻz-oʻzini boshqaruvchi uyushma.'
+      },
+      {
+        question: "Xadisi Shariflarni saxix xadislar sifatida to`plagan vatandoshlarimiz?",
+        answer: 'Imom Termiziy, Imom Buxoriy'
+      },
+      {
+        question: "Millatchilik nima?",
+        answer: 'Millatchilik deb millatni jamiyat birligining eng yuqori shakli va davlat shakllanishida birlamchi deb qarovchi siyosiy  yoki gʻoyaviy yoʻnalishga aytiladi.'
+      },
+      {
+        question: "Birinchi Prezident I.Karimov ta'kidlaganidek «G`oyaga qarshi faqat g`oya, fikrga qarshi faqat fikr, jaholatga qarshi… bilan kurash olib bormog`imiz lozim» nuqtalar o`rnini to`ldiring.",
+        answer: 'Prezidentimiz ilgari surayotgan fikr “ g\'oyaga qarshi g\'oya, jaholatga qarshi ma\'rifat bilan kurashishni olib bormog’imiz lozim”'
+      },
+      {
+        question: "O`zbekiston Respublikasi BMT ga qachon a'zolikka qabul qilindi?",
+        answer: '1992 yil 2-mart'
+      },
+      {
+        question: "Vijdon erkinligi nima?",
+        answer: 'Vijdon erkinligi – fuqarolarning istagan dinga e\'tiqod qilish yoki hech qanday dinga e\'tiqod qilmaslik huquqi'
+      },
+      {
+        question: "Mustaqil O`zbekiston Rеspublikasining Konstitutsiyasi qachon qabul qilingan?",
+        answer: '1992-yil  8-dekabr'
+      },
+      {
+        question: "Fuqarolarning vijdon erkinligi huquqi O`zbekiston Respublikasi Konstitutsiyasining qaysi moddasida aks ettirilgan?",
+        answer: '35-modda, 3-modda'
+      },
+      {
+        question: "O‘zbekiston Respublikasi Konstitutsiyasi necha bo‘limdan iborat?",
+        answer: '6 ta bo’lim'
+      },
+      {
+        question: "O`zbekistonda davlat boshqaruvi?",
+        answer: 'Demokratiya'
+      },
+      {
+        question: "Vijdon erkinligi nima?",
+        answer: 'Fuqarolarning istagan dinga e\'tiqod qilish yoki hech qanday dinga e\'tiqod qilmaslik huquqi'
+      },
+      {
+        question: "Millatchilik nima?",
+        answer: 'Millatchilik deb millatni jamiyat birligining eng yuqori shakli va davlat shakllanishida birlamchi deb qarovchi siyosiy  yoki gʻoyaviy yoʻnalishga aytiladi.'
+      },
+      {
+        question: "Bugungi kunda mamlakatimizda qanchadan ziyod millat va elat vakillari istiqomat qiladi?",
+        answer: '130 dan ziyod'
+      },
+      {
+        question: "«Kimki bir ko`ngli buzuqning xotirin shod aylagay Oncha borki, Ka'ba vayron bulsa obod aylagay». «Dil ba yoru, dast ba kor» tamoyilining muallifi kim?",
+        answer: 'Alisher Navoiy'
+      },
+      {
+        question: "Muhammad S.A.V.qachon o’zini Alloxning rasuli (elchisi) deb e’lon qildi?",
+        answer: '40 yoshlarida tahminam 610-yili'
+      },
+      {
+        question: "Islomda kimlar to’g’ri xalifalar deb hisoblanadi?",
+        answer: 'Ali ibn Abu Tolib, Usmon, Abu Bakr Siddiq, Umar'
+      },
+      {
+        question: "Qur’onda nechta sura bor?",
+        answer: '114 ta'
+      },
+      {
+        question: "Qur’on suralari qanday qismlarga bo’linadi?",
+        answer: 'Hijratdan oldin tushgan –‘Makka suralari’ Hijratdan keyin tushgan –‘Madina suralari’'
+      },
+      {
+        question: "O’rta Osiyo va Qozog’iston musulmonlari diniy boshqarmasi qachon tashkil topgan?",
+        answer: '1943-yil 31-iyul'
+      },
+      {
+        question: "O’zbekiston Konstitusiyasining qaysi moddalari din masalalariga bag’ishlangan?",
+        answer: '35, 71, 75-moddalar'
+      },
+      {
+        question: "Jahon dinlari quyidagilardan qaysilari?",
+        answer: 'Islom, Xristianlik, Buddaviylik'
+      },
+      {
+        question: "Hadis nima?",
+        answer: 'Muhammad(s.a.v)ning aytgan so’zlari va qilgan ishlari'
+      },
+      {
+        question: "Islom dini qachon paydo bo’ldi?",
+        answer: '610-yil deb hisoblash mumkin lekin aniq emas'
+      },
+      {
+        question: "Musulmonlarda xijriy yil hisobi qachon boshlanadi?",
+        answer: '622-yil 16-iyul juma kuni'
+      },
+      {
+        question: "Islom dinining Muqaddas kitobi nima deb ataladi?",
+        answer: 'Qur’oni karim'
+      },
+      {
+        question: "Shariatning mohiyati nima?",
+        answer: 'Islom huquq tizimi'
+      },
+      {
+        question: "Musulmon huquqshunosligidagi Shariat manbalari qaysilar?",
+        answer: 'Qur’on, sunna, ijmo, qiyos, urf-odatlar'
+      },
+      {
+        question: "Islomda ro’za necha kun tutiladi?",
+        answer: 'har yili ramazon oyida 30 kun'
+      },
+      {
+        question: "Islomning qanday asosiy yo’nalishlari bor?",
+        answer: 'Sunilik va Shialik'
+      },
+      {
+        question: "O’zbekiston Respublikasining «Vijdon erkinligi va diniy tashkilotlar to’g’risida»gi qonuni qaysi yilda kabul kilindi?",
+        answer: '1991-yil 14-iyunda'
+      },
+      {
+        question: "O’zbekiston Respublikasi Konstitusiyasining qaysi moddasida diniy tashkilotlarning davlatdan ajralganligi to’g’risidagi norma qayd qilingan?",
+        answer: 'Konstitutsiya 61-modda'
+      },
+      {
+        question: "Vijdon erkinligi mohiyati nima?",
+        answer: 'Har bir inson xohlagan dinga e\'tiqod qilish yoki hech qaysi dinga e\'tiqod qilmaslik huquqiga egaligi.'
+      },
+      {
+        question: "Islom ilohiyotining muhim soxasi - musulmon huquqshunosligi qanday nomlanadi?",
+        answer: 'Fiqh'
+      },
+      {
+        question: "Yovuz g`oya va mafkuralarning eng ko`p tarqalgan shakli qaysi?",
+        answer: 'Terorizm'
+      },
+      {
+        question: "Olamdagi jonsiz narsalarni ilohiylashtirish qaysi dinning xususiyati?",
+        answer: 'Buddaviylik yoki Animizm va Panteizm'
+      },
+      {
+        question: "O’zbekiston Respublikasi «Vijdon erkinligi va diniy tashkilotlar to’g’risida»gi Qonunining yangi tahriri qachon qabul qilindi?",
+        answer: '1998-yil 1-mayda'
+      },
+      {
+        question: "\"Bu dunyoni deb u dunyoni, oxiratni deb bu dunyoni unutmang\" degan ibora qaysi asardan olingan?",
+        answer: 'Islom Karimovning “Yuksaak ma’naviyat yengilmas kuch” asaridan'
+      },
+      {
+        question: "“Avesto” so’zining ma'nosi nima?",
+        answer: 'Parfiyoncha “apastak-matn” ko’pincha “Zend-Avesto” ya’ni “tafsir qilingan matn” deb ataladi'
+      },
+      {
+        question: "“Avesto”ning 2700 yilligi qachon nishonlangan?",
+        answer: '2001-yili'
+      },
+      {
+        question: "“Kimki bir ko`ngli buzuqning xotirin shod aylagay Oncha borki, Ka'ba vayron bulsa obod aylagay” ibora kimga tegishli?",
+        answer: 'Alisher Navoiy'
+      },
+      {
+        question: "2007 yili Toshkent shahriga qaysi tashkilot tomonidan \"Islom madaniyati poytaxti\" nomi berildi?",
+        answer: 'AYSESKO (ISESCO)'
+      },
+      {
+        question: "Marg’iloniyning \"Xidoya\" asarining ma'nosi nima?",
+        answer: '"to’g’ri yo’l"'
+      },
+      {
+        question: "Marg’iloniyning qaysi asari VIII asrdan buyon mukammal huquqiy manba sifatida o’z ahamiyatini yo’qotmagan?",
+        answer: '"Xidoya" asari'
+      },
+      {
+        question: "Toshkent Islom Universiteti qachon tashkil etilgan?",
+        answer: '1999-yili'
+      },
+      {
+        question: "Zardo’shtiylik dinida ezgulik mabudi deb kimni aytadi?",
+        answer: 'Ahura Mazda'
+      },
+      {
+        question: "Zardushtiylik dini qachon yaratilgan?",
+        answer: 'miloddan avalgi VII-VI asrlarda'
+      },
+      {
+        question: "Zardushtiylik iymoni 3 ta tayanch asosga tayanadi bular qaysilar?",
+        answer: 'fikrlar sofligiga, so’zning sobitligiga, amaldorlarning insoniyligiga'
+      },
+      {
+        question: "Islom dini ta’limotining asosi?",
+        answer: 'Imon, Namoz, Zakot, Ro’za, Hajj'
+      },
+      {
+        question: "O’zbekistonda islom dini bo’yicha olib boriladigan tadbirlarni qaysi tashkilot shug’illanadi?",
+        answer: 'O’zbekiston Musulmonlar idorasi'
+      },
+      {
+        question: "Prezidentimiz Sh.Mirziyoyev 2017 yil 20 sentyabr kuni BMT Bosh Assambliyasida qanday rezalyutsiyani taqdim qildi?",
+        answer: '"Marifat va diniy bag’rikenglik"'
+      },
+      {
+        question: "Prezidentimiz Sh.Mirziyoyev 2017 yil 20 sentyabr kuni BMT Bosh Assambliyasida taklif etilgan rezalyutsiya BMT tomonidan qachon qabul qilindi?",
+        answer: '2018-yil 12-dekabr'
+      },
+      {
+        question: "Markaziy Osiyo xalqlari islomdan oldin asosan qaysi dinga e’tiqod qilishgan?",
+        answer: 'Zardushtiylik'
+      },
+      {
+        question: "“Diling Ollohda, qo`ling mehnatda bo`lsin” hikmatining muallifi kim?",
+        answer: 'Bahouddin Naqshband'
+      },
+      {
+        question: "“Tarbiya biz uchun yo hayot - yo momot, yo najot, yo saodat - yo falokat masalasi” so`zlari qaysi ma'rifatparvar olim qalamiga mansub?",
+        answer: 'Abdulla Avloniy'
+      },
+      {
+        question: "“Ezgu fikr, ezgu so`z, ezgu amal qilish” qaysi ta'limotning bosh g`oyasi hisoblanadi?",
+        answer: 'Zardushtiylik'
+      },
+      {
+        question: "Markaziy Osiyo musulmonlari qaysi mazxabga mansub?",
+        answer: 'Sunilikning xanafiylik mazhabi'
+      },
+      {
+        question: "Mentalitet so‘zi lotinchadan tarjima qilinganda qanday ma’noni bildiradi?",
+        answer: 'Aql, fahm, zahn, idrok ma’nolarini'
+      },
+      {
+        question: "Hozirgi O‘zbekiston o‘rta asrlarda qanday nom bilan atalgan?",
+        answer: 'Movarounnahr'
+      },
+      {
+        question: "Yurtimizda Qatag‘on qurbonlarini yod etish kuni?",
+        answer: '31-avgust'
+      },
+      {
+        question: "Nechanchi yilda Shahidlar xotirasi xiyoboni tashkil etilgan?",
+        answer: '2000-yil'
+      },
+      {
+        question: "2008-yil qanday nomlangan?",
+        answer: 'Yoshlar yili'
+      },
+      {
+        question: "“Al jome’ as-sahih” asarining muallifi kim?",
+        answer: 'Imom Buxoriy'
+      },
+      {
+        question: "Arab grammatikasini mukammal ishlab chiqqan buyuk siymo?",
+        answer: 'Mahmud Zamaxshariy'
+      },
+      {
+        question: "Sharqda Shayx-ul rais nomi bilan ataluvchi alloma?",
+        answer: 'Abu Ali ibn Sino'
+      },
+      {
+        question: "O‘zbekiston Respublikasi Davlat mathiyasi mualliflari…?",
+        answer: 'Abdulla Oripov'
+      },
+      {
+        question: "O‘zbekiston Respublikasi Mustaqillik deklaratsiyasi qachon qabul qilingan?",
+        answer: '1990-yilling 20-iyun'
+      },
+      {
+        question: "... Gar qilich boshimga ham kelsa degayman rostin! Misralarini muallifini aniqlang ?",
+        answer: 'Muqumiy'
+      },
+      {
+        question: "Muhabbatsiz kishi odam emasdur, Gar odamsan muhabbat ixtiyor et! - Ушбу сатрлар муаллифини аниқланг?",
+        answer: 'Nodira'
+      },
+      {
+        question: "Diniy tashkilot nima?",
+        answer: 'Bu ma\'lum dinga ishonuvchilar va ularning jamoalarining uyushmasidir'
+      },
+      {
+        question: "Eng qadimgi jahon dini qaysi?",
+        answer: 'Buddizm'
+      },
+      {
+        question: "Buddizm dini qachon paydo bo’ldi?",
+        answer: 'Miloddan avvalgi VI-V asrlarda'
+      },
+      {
+        question: "Xadisi Shariflarni sahih hadislar sifatida to`plagan vatandoshlarimiz?",
+        answer: 'Imom Termiziy'
+      },
+      {
+        question: "Xristianlikning o’gil-hudo Iso Masixga e’tiqod qilish bilan bog’liq asosiy aqidasi qaysi ?",
+        answer: 'Mujassamlashish aqidasi'
+      },
+      {
+        question: "Xristianlik dinidagi bo’linish qachon sodir bo’ldi?",
+        answer: '1054-yillar'
+      },
+      {
+        question: "Sharq dinlaridan qaysi biri xristianlikka eng ko’p ta’sir ko’rsatdi?",
+        answer: 'Iudaizm'
+      },
+      {
+        question: "Qurbon hayiti qachon bayram qilinadi?",
+        answer: 'Zul-Hidja oyining 10-kuni'
+      },
+      {
+        question: "Shialiklarda Muqaddas hadislar nima deb ataladi?",
+        answer: '"Kutub al-Arba’a" yoki "4 kitob"'
+      },
+      {
+        question: "Islom diniy marosimlarni olib boruvchi ruxoniy qanday ataladi?",
+        answer: 'Imom, Ulamo, Mulla'
+      },
+      {
+        question: "So’fiylarning eng asosiy maqsadi nima?",
+        answer: 'Dunyoviy bema\'nilikdan xoli, tabiatidagi salbiy sifatlardan yuqoriga ko\'tarila olgan "komil inson" tarbiyalashdir'
+      },
+      {
+        question: "Islomni modernizasiya (isloh) qilishining mohiyati nima?",
+        answer: 'Islomni modernizatsiya qilish - bu zamonaviy talablar va ehtiyojlarga javoban islom ta\'limotlari va amaliyotlarini o\'zgartirish va yangilash jarayoni'
+      },
+      {
+        question: "Asosan bir elat yoki millat vakillari qanday dinlarga e’tiqod qiladi?",
+        answer: 'Ajdodlari e’tiqod qilgan dinlarga'
+      }
+]
+
+
+let searchQuestion = document.getElementById("searchQuestion");
+let mainbox = document.getElementById("mainbox");
+let theQues = false;
+let theAns = false;
+
+let idval = 0;
+quesans.forEach(element => {
+    idval++;
+    element.uniqueid = `id${idval}`;
+    theQues = document.createElement("h1");
+    theAns = document.createElement("p");
+    theQues.classList.add(element.uniqueid)
+    theAns.classList.add(`class${element.uniqueid}`)
+    theQues.innerText = element.question;
+    theAns.innerText = element.answer;
+    mainbox.appendChild(theQues);
+    mainbox.appendChild(theAns);
+});
+
+let myArr = [];
+
+searchQuestion.addEventListener("input", ()=> {
+    console.log(myArr);
+
+    let myval = searchQuestion.value;
+    myval = myval.toLowerCase();
+
+    quesans.forEach(element => {
+
+        element.question = element.question.toLocaleLowerCase();
+
+        if (element.question.includes(myval)) {
+
+            if (!myArr.includes(element)) {
+                myArr.push(element)
+            }
+
+        } else{
+            let indexim = myArr.indexOf(element);
+            if (document.querySelector(`.${element.uniqueid}`)) {
+                document.querySelector(`.${element.uniqueid}`).style.display = "none";
+                document.querySelector(`.class${element.uniqueid}`).style.display = "none";
+            }
+
+            if (indexim !== -1) {
+            // Remove the element using splice
+            myArr.splice(indexim, 1);
+            }
+        };
+
+    });
+
+    myArr.forEach(element => {
+        if (element) {
+            if (document.querySelector(`.${element.uniqueid}`)) {
+                document.querySelector(`.${element.uniqueid}`).style.display = "block";
+                document.querySelector(`.class${element.uniqueid}`).style.display = "block";
+
+            }
+          }
+
+    });
+})
